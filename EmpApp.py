d7f2fad270f30af22b44055fd6a868da09fa62ba
dovi777 <74944061+dovi777@users.noreply.github.com>
2023-05-05 00:31:59 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -38,7 +38,6 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
 
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
