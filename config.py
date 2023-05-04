9ca626b6e6bc9c5b521a21536bf8b27a7e7904d5
dovi777 <74944061+dovi777@users.noreply.github.com>
2023-05-05 00:33:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "db1.cyje99iv5xjs.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "cloudAssignment"
-customdb = "db1"
+customdb = "DB1"
 customregion = "us-east-1"
 
